52794020f7e6632bdca95c58adf9e625ad0a3a3c
Jaures-Beinjamin <jauresbeinjamin@gmail.com>
2025-12-28 14:22:54 +0100

    feat(util): ajout de la vérification des collisions des navires

---
+++ src/util/ShipCollisionChecker.scala
@@ -0,0 +1,17 @@
+package util
+
+import model.Ship
+
+object ShipCollisionChecker {
+
+  /** Vérifie si le nouveau navire chevauche un navire existant */
+  def isCollision(newShip: Ship, existingShips: Set[Ship]): Boolean = {
+    existingShips.exists { ship =>
+      ship.positions.exists(pos => newShip.positions.contains(pos))
+    }
+  }
+
+  /** Vérifie si toutes les positions sont libres pour un navire */
+  def canPlaceShip(newShip: Ship, existingShips: Set[Ship]): Boolean =
+    !isCollision(newShip, existingShips)
+}
